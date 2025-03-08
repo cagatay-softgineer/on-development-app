b3577fddba18e681f60c976537466de4a056463d
Çağatay Alkan <cagatayalkan333@gmail.com>
2025-03-08 16:24:57 +0300

    Update linked_app.dart

--- ssdk_rsrc/lib/models/linked_app.dart
+++ ssdk_rsrc/lib/models/linked_app.dart
@@ -10,9 +10,9 @@ class LinkedApp {
 
   LinkedApp({
     required this.name, // Name must be provided
-    this.buttonText = "Loading...", // Default button text
+    this.buttonText = "Checking...", // Default button text
     this.userPic = "", // Default user picture URL
-    this.userDisplayName = "Loading...", // Default display name
+    this.userDisplayName = "Checking...", // Default display name
     ButtonParams? appButtonParams, // Nullable to allow default instantiation
     this.isLinked = false, // Default linked state
   }) : appButtonParams = appButtonParams ?? ButtonParams(); // Default to new instance
