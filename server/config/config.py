cb7b4779d6a20da6c5b0c8b47a368ce95bbf2041
Çağatay Alkan <cagatayalkan333@gmail.com>
2025-04-12 05:21:00 +0300

    QA Workflow

--- server/config/config.py
+++ server/config/config.py
@@ -4,12 +4,6 @@ import secrets
 
 class Settings(BaseSettings):
     jwt_secret_key: str = Field(..., env="JWT_SECRET_KEY")
-    sql_server_host: str = Field(..., env="SQL_SERVER_HOST")
-    sql_server_port: str = Field(..., env="SQL_SERVER_PORT")
-    sql_server_database: str = Field(..., env="SQL_SERVER_DATABASE")
-    sql_server_user: str = Field(..., env="SQL_SERVER_USER")
-    sql_server_password: str = Field(..., env="SQL_SERVER_PASSWORD")
-    secondary_sql_database: str = Field(..., env="SSQL_SERVER_DATABASE")
     spotify_client_id: str = Field(..., env="SPOTIFY_CLIENT_ID")
     spotify_client_secret: str = Field(..., env="SPOTIFY_CLIENT_SECRET")
     auth_redirect_uri: str = Field(..., env="AUTH_REDIRECT_URI")
@@ -42,19 +36,6 @@ class FirebaseConfig(BaseSettings):
         env_file_encoding = "utf-8"
 
 firebase_config = FirebaseConfig()
-
-class SQLServerConfig(BaseSettings):
-    sql_server_host: str = Field(..., env="SQL_SERVER_HOST")
-    sql_server_port: str = Field(..., env="SQL_SERVER_PORT")
-    sql_server_database: str = Field(..., env="SQL_SERVER_DATABASE")
-    sql_server_user: str = Field(..., env="SQL_SERVER_USER")
-    sql_server_password: str = Field(..., env="SQL_SERVER_PASSWORD")
-    
-    class Config:
-        env_file = ".env"
-        env_file_encoding = "utf-8"
-    
-sql_config = SQLServerConfig()
 
 # db = init_firebase(firebase_config)
 # converter = SQLToFirestoreConverter(db, alias_map=sql2firebase.alias_map)
