0567e93217950f7c53773a94d9ee98c5bd90ea5d
Çağatay Alkan <cagatayalkan333@gmail.com>
2025-04-13 23:25:40 +0300

    Update server.py

--- server/server.py
+++ server/server.py
@@ -32,14 +32,15 @@ from Blueprints.lyrics import lyrics_bp
 from Blueprints.youtube_music import youtubeMusic_bp
 import argparse
 
-check_log_folder()
-load_JSONs()
 
 gui = CmdGUI()
 
 
 def create_app(testing=False):
 
+    check_log_folder()
+    load_JSONs()
+    
     app = Flask(__name__)
 
     jwt = JWTManager(app)  # noqa: F841
