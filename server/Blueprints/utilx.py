13b2f78f7c44d640e06e461eb72d2b80f7d293fa
Çağatay Alkan <cagatayalkan333@gmail.com>
2025-04-14 02:13:07 +0300

    Remove Logs Trend Graph Endpoint

--- server/Blueprints/utilx.py
+++ server/Blueprints/utilx.py
@@ -1,15 +1,9 @@
 from flask import Blueprint, render_template, jsonify, request, current_app
 from util.logit import get_logger
-from util.error_handling import log_error
 from util.utils import (
     route_descriptions,
     parse_logs_from_folder,
-    parse_logs_to_dataframe,
 )
-import pandas as pd
-import json
-import plotly.graph_objects as go
-from plotly.utils import PlotlyJSONEncoder
 from util.authlib import requires_scope
 
 util_bp = Blueprint("util", __name__)
@@ -68,69 +62,6 @@ def visualize_logs():
         log_type_filter=log_type_filter,
         filename_filter=filename_filter,
     )
-
-
-# Endpoint to display the trend chart
-@util_bp.route("/logs/trend", methods=["GET"])
-@requires_scope("admin")
-def logs_trend_chart():
-    """
-    This function generates a trend chart of log types over time.
-
-    Parameters:
-    None
-
-    Returns:
-    tuple: A tuple containing the rendered template with the log trend chart,
-    or a JSON response with an error message and a status code of 404 if no valid logs are available.
-    """
-    try:
-        logs_folder_path = "logs"  # Replace with your actual folder path
-        df = parse_logs_to_dataframe(logs_folder_path)
-
-        if df.empty:
-            return "No valid logs available to display.", 404
-
-        # Group by time intervals and log type, then count occurrences
-        df["timestamp"] = pd.to_datetime(
-            df["timestamp"]
-        )  # Ensure timestamp is in datetime format
-        df.set_index("timestamp", inplace=True)
-        grouped = (
-            df.groupby([pd.Grouper(freq="1h"), "log_type"]
-                       ).size().unstack(fill_value=0)
-        )
-
-        # Create a Plotly figure
-        fig = go.Figure()
-
-        for log_type in grouped.columns:
-            fig.add_trace(
-                go.Scatter(
-                    x=grouped.index,
-                    y=grouped[log_type],
-                    mode="lines+markers",
-                    name=log_type,
-                )
-            )
-
-        # Add chart details
-        fig.update_layout(
-            title="Log Type Trend Over Time",
-            xaxis_title="Time (Hourly)",
-            yaxis_title="Count",
-            legend_title="Log Type",
-            template="plotly_white",
-            hovermode="x unified",
-        )
-        # Convert the Plotly figure to JSON
-        graph_json = json.dumps(fig, cls=PlotlyJSONEncoder)
-
-        return render_template("plotly_chart.html", graph_json=graph_json)
-    except Exception as e:
-        log_error(e)
-        return render_template("plotly_chart.html", graph_json=graph_json)
-
 
 @util_bp.route("/endpoints")
 @requires_scope("admin")
